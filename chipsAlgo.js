2fb220474297f6164928b6253fff0064c325a1fe
JasperFlame <samsonov.akimbo@gmail.com>
2017-08-07 16:27:56 +0500

    fix #2

--- chipsAlgo.js
+++ chipsAlgo.js
@@ -1,22 +1,39 @@
-//
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
 
-var manaDices = Math.pow(2,9);
+</body>
+</html>
+
+<script>
+  var manaDices = Math.pow(2,9);
 var weapons = Math.pow(6,4);
 var element = Math.pow(6,10);
 
 
-var a = Math.round((manaDices * weapons - 1) * getRandom(0, 1));               
-var b = Math.round((element - 1) * getRandom(0, 1));
-
+var a = Math.round((manaDices * weapons - 1) * getRandom(0, 1));        
 var aTrim = a / manaDices;
-
 var aInt = aTrim - aTrim % 1;
 var aDivisor = a % manaDices;
-
 var a1 = toBase(aInt, 6, 0)
 var a2 = toBase(aDivisor, 2, 0);
+  
+  
+var b = Math.round((element - 1) * getRandom(0, 1));
+var bTrim = b / manaDices;
+var bInt = bTrim - bTrim % 1;
+var bDivisor = b % manaDices;
+var b1 = toBase(bInt, 6, 0);
+var b2 = toBase(bDivisor, 2,0);
 
-document.write(aDivisor);
+
+
+document.write(b1);
 
 function toBase(number, radix, digits) {
     if (digits && digits.length >= radix)
@@ -30,3 +47,5 @@ function toBase(number, radix, digits) {
 function getRandom(min, max) {
     return Math.random() * (max - min) + min;
 }
+
+</script>

956b05972d26b446bf9e5e918909945e9f1eb422
Max Samsonov <samsonov.akimbo@gmail.com>
2017-08-08 18:36:54 +0500

    #Test

--- chipsAlgo.js
+++ chipsAlgo.js
@@ -1,30 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>JS Bin</title>
-</head>
-<body>
-
-</body>
-</html>
-
-<script>
   var manaDices = Math.pow(2,9);
 var weapons = Math.pow(6,4);
 var element = Math.pow(6,10);
 
 
-var a = Math.round((manaDices * weapons - 1) * getRandom(0, 1));        
-var aTrim = a / manaDices;
-var aInt = aTrim - aTrim % 1;
+var A = Math.ceil((manaDices * weapons) * getRandom(0.1, 1));  
+var _a = Math.ceil(A / manaDices);
+var _aMod = A % manaDices;
+var _a1 = convertFromBaseToBase(_a, 10, 6);
+
+document.write("RND: " + A + "<br/ >");
+document.write("FULL: " + _a+ "<br/ >");
+document.write("MOD: " + _aMod+ "<br/ >");
+document.write("A1: " + _a1+ "<br/ >");
+
+var a = aRnd / manaDices;
+var aInt = a - a % 1;
+
 var aDivisor = a % manaDices;
 var a1 = toBase(aInt, 6, 0)
 var a2 = toBase(aDivisor, 2, 0);
   
   
-var b = Math.round((element - 1) * getRandom(0, 1));
+var b = Math.round(element * getRandom(0.1, 1));
 var bTrim = b / manaDices;
 var bInt = bTrim - bTrim % 1;
 var bDivisor = b % manaDices;
@@ -33,7 +30,10 @@ var b2 = toBase(bDivisor, 2,0);
 
 
 
-document.write(b1);
+function convertFromBaseToBase(str, fromBase, toBase){
+	var num = parseInt(str, fromBase);
+    return num.toString(toBase);
+}
 
 function toBase(number, radix, digits) {
     if (digits && digits.length >= radix)
@@ -47,5 +47,3 @@ function toBase(number, radix, digits) {
 function getRandom(min, max) {
     return Math.random() * (max - min) + min;
 }
-
-</script>
